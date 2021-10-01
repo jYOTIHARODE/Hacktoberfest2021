52a5683b4bbc8b3d7610c0c44c68075324eccf7f
Saurav Gupta <67512092+sauravgpt2@users.noreply.github.com>
2021-10-01 13:52:02 +0530

    Christmas Tree made with Flutter

---
+++ christmas_tree/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:christmas_tree/screen/christmas.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Christmas(),
+    );
+  }
+}
