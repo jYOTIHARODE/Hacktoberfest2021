9335cfb2715e1f5caf7624f6e1d631db260fbc26
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2021-10-07 14:43:40 +0530

    Create ASCII.cpp

---
+++ CPP/ASCII.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int a;
+	char ch;
+	cout<<"American Standard Code for Information Interchange(ASCII)"<<endl;
+	cout<<"Enter a character: ";
+	cin>>ch;
+	a=ch;
+	cout<<"\nThe number of "<<ch<<" in ASCII is "<<a<<endl;
+	
+	return 0;
+}
